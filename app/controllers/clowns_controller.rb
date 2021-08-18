7a3fe09104473e0958f07a923564905a89869a5b
Etienne Tessier <etienne_tessier100@hotmail.ca>
2021-08-18 12:04:36 -0400

    started working on the remaining methods in clowns controller

--- app/controllers/clowns_controller.rb
+++ app/controllers/clowns_controller.rb
@@ -2,7 +2,22 @@ class ClownsController < ApplicationController
   def index
     @clowns = Clown.all
   end
+
   def show
     @clown = Clown.find(params[:id])
+  end
+
+  def new
+    @clown = Clown.new
+  end
+
+  def create
+    @clown = Clown.new(params[:id])
+
+    if @clown.save
+      redirect_to @clown, notice: 'clown was successfully created.'
+    else
+      render :new
+    end
   end
 end
