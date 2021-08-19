4a7963ce51fe52256e9d0173a3f8bc30ada54720
Etienne Tessier <etienne_tessier100@hotmail.ca>
2021-08-19 08:38:46 -0400

    installed active storage

--- app/models/clown.rb
+++ app/models/clown.rb
@@ -1,6 +1,8 @@
 class Clown < ApplicationRecord
+  has_one_attached :photo
+
   belongs_to :user
   has_many :bookings
 
-  # validates :name, :years_of_experience, :gender, :rental_duration, presence: true
+  # validates :name, :years_of_experience, :gender, :price presence: true
 end
