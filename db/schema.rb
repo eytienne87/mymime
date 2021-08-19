e80f48877ffde83246b8e2630024df976daffe9b
Etienne Tessier <etienne_tessier100@hotmail.ca>
2021-08-19 13:35:20 -0400

    Added a description attribute to the clowns model and removed the gender attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_123024) do
+ActiveRecord::Schema.define(version: 2021_08_19_173327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,12 +50,12 @@ ActiveRecord::Schema.define(version: 2021_08_19_123024) do
   create_table "clowns", force: :cascade do |t|
     t.string "name"
     t.integer "years_of_experience"
-    t.string "gender"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.string "image_url"
     t.integer "price"
+    t.text "description"
     t.index ["user_id"], name: "index_clowns_on_user_id"
   end
 
